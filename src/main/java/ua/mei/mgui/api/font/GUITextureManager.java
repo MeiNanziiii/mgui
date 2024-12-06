4d63a785ae28c1297c42e248857683cdc1625d07
MeiNanziiii <meinanziiii.ua@gmail.com>
2024-12-06 11:43:18 +0200

    gui stuff

---
+++ src/main/java/ua/mei/mgui/api/font/GUITextureManager.java
@@ -0,0 +1,12 @@
+package ua.mei.mgui.api.font;
+
+import net.minecraft.text.MutableText;
+import ua.mei.pfu.api.font.BitmapGlyph;
+import ua.mei.pfu.api.font.FontResourceManager;
+
+public class GUITextureManager {
+    public static MutableText requestGui(String path, FontResourceManager manager) {
+        BitmapGlyph glyph = manager.requestGlyph(path, 256, 0);
+        return glyph.formatter().space(-8, -glyph.glyphWidth + 7).value;
+    }
+}
---
+++ src/main/java/ua/mei/mgui/impl/VanillaTextures.java
@@ -0,0 +1,51 @@
+package ua.mei.mgui.impl;
+
+import net.minecraft.screen.ScreenHandlerType;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.ApiStatus;
+import ua.mei.mgui.api.font.GUITextureManager;
+import ua.mei.pfu.api.font.FontResourceManager;
+
+import static ua.mei.mgui.impl.MGuiImpl.MOD_ID;
+
+@ApiStatus.Internal
+public class VanillaTextures {
+    public static FontResourceManager MANAGER;
+
+    public static MutableText GENERIC_9X6;
+    public static MutableText GENERIC_9X5;
+    public static MutableText GENERIC_9X4;
+    public static MutableText GENERIC_9X3;
+    public static MutableText GENERIC_9X2;
+    public static MutableText GENERIC_9X1;
+
+    public static void load() {
+        MANAGER = FontResourceManager.create(MOD_ID, MOD_ID, "vanilla");
+
+        GENERIC_9X6 = GUITextureManager.requestGui("gui/container/generic_9x6.png", MANAGER);
+        GENERIC_9X5 = GUITextureManager.requestGui("gui/container/generic_9x5.png", MANAGER);
+        GENERIC_9X4 = GUITextureManager.requestGui("gui/container/generic_9x4.png", MANAGER);
+        GENERIC_9X3 = GUITextureManager.requestGui("gui/container/generic_9x3.png", MANAGER);
+        GENERIC_9X2 = GUITextureManager.requestGui("gui/container/generic_9x2.png", MANAGER);
+        GENERIC_9X1 = GUITextureManager.requestGui("gui/container/generic_9x1.png", MANAGER);
+    }
+
+    public static MutableText fromScreenHandler(ScreenHandlerType<?> handler) {
+        if (handler == ScreenHandlerType.GENERIC_9X1) {
+            return GENERIC_9X1;
+        } else if (handler == ScreenHandlerType.GENERIC_9X2) {
+            return GENERIC_9X2;
+        } else if (handler == ScreenHandlerType.GENERIC_9X3) {
+            return GENERIC_9X3;
+        } else if (handler == ScreenHandlerType.GENERIC_9X4) {
+            return GENERIC_9X4;
+        } else if (handler == ScreenHandlerType.GENERIC_9X5) {
+            return GENERIC_9X5;
+        } else if (handler == ScreenHandlerType.GENERIC_9X6) {
+            return GENERIC_9X6;
+        } else {
+            return Text.empty();
+        }
+    }
+}
